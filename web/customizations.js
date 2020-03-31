94fb5ef688e01bc4c909ce379800ac63ee096783
Matteo Depalo <matteo@depalo.me>
2020-03-31 19:14:02 +0100

    Update customizations.js

--- web/customizations.js
+++ web/customizations.js
@@ -1,13 +1,13 @@
 document.addEventListener("webviewerloaded", function() {
   PDFViewerApplication.initializedPromise.then(function() {
-    PDFViewerApplicationOptions.toolbar.download.setAttribute("hidden", "true");
-    PDFViewerApplicationOptions.toolbar.print.setAttribute("hidden", "true");
-    PDFViewerApplicationOptions.toolbar.openFile.setAttribute("hidden", "true");
-    PDFViewerApplicationOptions.toolbar.viewBookmark.setAttribute("hidden", "true");
+    PDFViewerApplication.appConfig.toolbar.download.setAttribute("hidden", "true");
+    PDFViewerApplication.appConfig.toolbar.print.setAttribute("hidden", "true");
+    PDFViewerApplication.appConfig.toolbar.openFile.setAttribute("hidden", "true");
+    PDFViewerApplication.appConfig.toolbar.viewBookmark.setAttribute("hidden", "true");
     
-    PDFViewerApplicationOptions.secondaryToolbar.openFileButton.setAttribute("hidden", "true");
-    PDFViewerApplicationOptions.secondaryToolbar.printButton.setAttribute("hidden", "true");
-    PDFViewerApplicationOptions.secondaryToolbar.downloadButton.setAttribute("hidden", "true");
-    PDFViewerApplicationOptions.secondaryToolbar.viewBookmarkButton.setAttribute("hidden", "true");
+    PDFViewerApplication.appConfig.secondaryToolbar.openFileButton.setAttribute("hidden", "true");
+    PDFViewerApplication.appConfig.secondaryToolbar.printButton.setAttribute("hidden", "true");
+    PDFViewerApplication.appConfig.secondaryToolbar.downloadButton.setAttribute("hidden", "true");
+    PDFViewerApplication.appConfig.secondaryToolbar.viewBookmarkButton.setAttribute("hidden", "true");
   })
 });
